ce4b25f841b8a2708eb7321684219bb8d505ef31
Krupa Patel <krupapatel@academic.rrc.ca>
2025-11-05 13:22:00 -0600

    feat: fix branch data types for string IDs and phone format

--- src/data/branch.ts
+++ src/data/branch.ts
@@ -2,63 +2,63 @@ import { Branch } from "../models/branch";
 
 export const branches: Branch[] = [
   {
-    id: 1,
+    id: "1",
     name: "Vancouver Branch",
     address: "1300 Burrard St, Vancouver, BC, V6Z 2C7",
-    phone: "604-456-0022"
+    phone: "6044560022"
   },
   {
-    id: 2,
+    id: "2",
     name: "Edmonton Branch",
     address: "7250 82 Ave NW, Edmonton, AB, T6B 0G4",
-    phone: "780-468-6800"
+    phone: "7804686800"
   },
   {
-    id: 3,
+    id: "3",
     name: "Winnipeg Main Branch",
     address: "333 Portage Ave, Winnipeg, MB, R3B 2B4",
-    phone: "204-934-6400"
+    phone: "2049346400"
   },
   {
-    id: 4,
+    id: "4",
     name: "Brandon Branch",
     address: "1010 Victoria Ave, Brandon, MB, R7A 1A7",
-    phone: "204-729-3500"
+    phone: "2047293500"
   },
   {
-    id: 5,
+    id: "5",
     name: "Winnipeg East Branch",
     address: "850 Regent Ave W, Winnipeg, MB, R2C 3A8",
-    phone: "204-222-6200"
+    phone: "2042226200"
   },
   {
-    id: 6,
+    id: "6",
     name: "Winnipeg South Branch",
     address: "1910 Pembina Hwy, Winnipeg, MB, R3T 2G2",
-    phone: "204-269-7000"
+    phone: "2042697000"
   },
   {
-    id: 7,
+    id: "7",
     name: "Montreal Branch",
     address: "1200 Rue Sainte-Catherine O, Montréal, QC, H3B 1K1",
-    phone: "514-555-0100"
+    phone: "5145550100"
   },
   {
-    id: 8,
+    id: "8",
     name: "Toronto Branch",
     address: "200 Bay St, Toronto, ON, M5J 2J5",
-    phone: "416-555-0200"
+    phone: "4165550200"
   },
   {
-    id: 9,
+    id: "9",
     name: "Fredericton Branch",
     address: "440 King St, Fredericton, NB, E3B 5H8",
-    phone: "506-555-0300"
+    phone: "5065550300"
   },
   {
-    id: 10,
+    id: "10",
     name: "Steinbach Branch",
     address: "333 Main St, Steinbach, MB, R5G 1Z2",
-    phone: "204-555-0400"
+    phone: "2045550400"
   }
 ];
